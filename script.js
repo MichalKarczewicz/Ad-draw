08b5fba1d9473263b5b4d422a927a131217035b9
Michal Karczewicz <michu.karcz@wp.pl>
2023-08-11 17:00:59 +0200

    feat: add functionality to select ad type for random selection

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// leaderboard, large rectangle, medium rectangle, mobile banner, wide skyscraper
-// pg, jpeg, png, webp, avif
+// Array to store the data from json file 
+let data = [];
 
-const getAdsList = () => {
-    fetch('payload.json')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-         getRandomAd(data); 
-      })
-      .catch(error => {
-        console.error('Loading error', error);
-      });
+// Function to get data from json file
+async function getAdsList(){
+  try{
+    const response = await fetch('payload.json');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    data = await response.json();
+  }catch(error){
+    console.error('Loading error', error);
   }
-  
-const filterAdsByDomain = (banners) => {
-    const currentDomain = window.location.hostname;
+}
 
+const getCurrentDomain = (banners) => {
+  const currentDomain = window.location.hostname;
+  return currentDomain;
+}
+
+// Function to check the current domain and filter data
+const filterAdsByDomain = (banners, currentDomain) => {
     return banners.filter( ads => {
         let itemDomain = new URL(ads.link).hostname;
         return itemDomain !== currentDomain
@@ -32,17 +33,37 @@ const getRandomAdURL = (ads) => {
   return allAdsURLs[randomIndex];
 }
 
-const getRandomAd = (ads) =>{
-    const { banners } = ads;
-    const filteredAds = filterAdsByDomain(banners);
-    const randomAd = getRandomAdURL(filteredAds.map(item => Object.values(item.images)));
-    
-    return randomAd      
+const filterBannerByType = (filteredAds, type) => {
+  if(type === undefined){
+    return getRandomAdURL(filteredAds.map(item => Object.values(item.images)));
+  }else{
+    const imagesType = filteredAds.map(item => item.images[type])
+    .filter(value => value !== undefined)
+    .map(value => `${value}`);
+    return getRandomAdURL(imagesType)
+  }
 }
 
+/*
+  Single ad randomization function
+*/
+const getRandomAd = (data, type) =>{
+    const { banners } = data;
+    const filteredAds = filterAdsByDomain(banners, getCurrentDomain(banners));
+    const randomAd = filterBannerByType(filteredAds, type);
+
+    console.log("losowa reklama:", randomAd)
+    return randomAd 
+}
+
+async function main(){
+  await getAdsList();
+  getRandomAd(data);
+  getRandomAd(data, "mobile banner");
+}
 
 window.onload = () => {
-    getAdsList()
+    main();
 };
 
 // podejscie ze uzytkownik wprowadza ze chce reklame wylosowac typu 
